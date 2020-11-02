b737e7182048a406d8d5f6a9bb54e8e8c7e4d926
kairyou123 <kairyou522@gmail.com>
2020-11-02 14:43:18 +0700

    Them cac truong cho Entity HanhKhach

--- TourApp/Form/HanhKhach/HanhKhachAdd.Designer.cs
+++ TourApp/Form/HanhKhach/HanhKhachAdd.Designer.cs
@@ -57,12 +57,34 @@
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
+            this.Radio_GT = new System.Windows.Forms.RadioButton();
+            this.Radio_GT1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_cmnd = new System.Windows.Forms.TextBox();
+            this.txt_diachi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.txt_passport = new System.Windows.Forms.TextBox();
+            this.err_msg5 = new System.Windows.Forms.Label();
+            this.err_msg6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Controls.Add(this.err_msg6);
+            this.panel1.Controls.Add(this.err_msg5);
+            this.panel1.Controls.Add(this.txt_passport);
+            this.panel1.Controls.Add(this.label15);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txt_diachi);
+            this.panel1.Controls.Add(this.txt_cmnd);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Radio_GT1);
+            this.panel1.Controls.Add(this.Radio_GT);
             this.panel1.Controls.Add(this.err_msg4);
             this.panel1.Controls.Add(this.err_msg3);
             this.panel1.Controls.Add(this.err_msg2);
@@ -86,7 +108,7 @@
             // 
             this.err_msg4.AutoSize = true;
             this.err_msg4.ForeColor = System.Drawing.Color.Red;
-            this.err_msg4.Location = new System.Drawing.Point(218, 257);
+            this.err_msg4.Location = new System.Drawing.Point(123, 257);
             this.err_msg4.Name = "err_msg4";
             this.err_msg4.Size = new System.Drawing.Size(44, 15);
             this.err_msg4.TabIndex = 7;
@@ -97,7 +119,7 @@
             // 
             this.err_msg3.AutoSize = true;
             this.err_msg3.ForeColor = System.Drawing.Color.Red;
-            this.err_msg3.Location = new System.Drawing.Point(171, 182);
+            this.err_msg3.Location = new System.Drawing.Point(80, 182);
             this.err_msg3.Name = "err_msg3";
             this.err_msg3.Size = new System.Drawing.Size(38, 15);
             this.err_msg3.TabIndex = 7;
@@ -108,7 +130,7 @@
             // 
             this.err_msg2.AutoSize = true;
             this.err_msg2.ForeColor = System.Drawing.Color.Red;
-            this.err_msg2.Location = new System.Drawing.Point(224, 112);
+            this.err_msg2.Location = new System.Drawing.Point(136, 112);
             this.err_msg2.Name = "err_msg2";
             this.err_msg2.Size = new System.Drawing.Size(38, 15);
             this.err_msg2.TabIndex = 7;
@@ -119,7 +141,7 @@
             // 
             this.err_msg1.AutoSize = true;
             this.err_msg1.ForeColor = System.Drawing.Color.Red;
-            this.err_msg1.Location = new System.Drawing.Point(224, 46);
+            this.err_msg1.Location = new System.Drawing.Point(136, 46);
             this.err_msg1.Name = "err_msg1";
             this.err_msg1.Size = new System.Drawing.Size(38, 15);
             this.err_msg1.TabIndex = 7;
@@ -140,46 +162,46 @@
             // 
             this.btn.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.btn.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btn.Location = new System.Drawing.Point(460, 335);
+            this.btn.Location = new System.Drawing.Point(605, 382);
             this.btn.Name = "btn";
             this.btn.Size = new System.Drawing.Size(120, 31);
-            this.btn.TabIndex = 5;
+            this.btn.TabIndex = 9;
             this.btn.Text = "Xác Nhận";
             this.btn.UseVisualStyleBackColor = false;
             this.btn.Click += new System.EventHandler(this.btn_Click);
             // 
             // txt_phone
             // 
-            this.txt_phone.Location = new System.Drawing.Point(156, 287);
+            this.txt_phone.Location = new System.Drawing.Point(36, 288);
             this.txt_phone.Name = "txt_phone";
-            this.txt_phone.Size = new System.Drawing.Size(226, 23);
+            this.txt_phone.Size = new System.Drawing.Size(272, 23);
             this.txt_phone.TabIndex = 3;
             this.txt_phone.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
             this.txt_phone.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
             // 
             // txt_email
             // 
-            this.txt_email.Location = new System.Drawing.Point(156, 207);
+            this.txt_email.Location = new System.Drawing.Point(38, 213);
             this.txt_email.Name = "txt_email";
-            this.txt_email.Size = new System.Drawing.Size(424, 23);
+            this.txt_email.Size = new System.Drawing.Size(270, 23);
             this.txt_email.TabIndex = 2;
             this.txt_email.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
             this.txt_email.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
             // 
             // txt_name
             // 
-            this.txt_name.Location = new System.Drawing.Point(156, 144);
+            this.txt_name.Location = new System.Drawing.Point(38, 143);
             this.txt_name.Name = "txt_name";
-            this.txt_name.Size = new System.Drawing.Size(424, 23);
+            this.txt_name.Size = new System.Drawing.Size(270, 23);
             this.txt_name.TabIndex = 1;
             this.txt_name.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
             this.txt_name.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
             // 
             // txt_id
             // 
-            this.txt_id.Location = new System.Drawing.Point(156, 75);
+            this.txt_id.Location = new System.Drawing.Point(38, 75);
             this.txt_id.Name = "txt_id";
-            this.txt_id.Size = new System.Drawing.Size(424, 23);
+            this.txt_id.Size = new System.Drawing.Size(270, 23);
             this.txt_id.TabIndex = 0;
             this.txt_id.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
             this.txt_id.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
@@ -188,7 +210,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.Info;
-            this.label2.Location = new System.Drawing.Point(116, 182);
+            this.label2.Location = new System.Drawing.Point(38, 182);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 15);
             this.label2.TabIndex = 3;
@@ -198,7 +220,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.Info;
-            this.label1.Location = new System.Drawing.Point(116, 257);
+            this.label1.Location = new System.Drawing.Point(38, 257);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(79, 15);
             this.label1.TabIndex = 2;
@@ -208,7 +230,7 @@
             // 
             this.LBL1.AutoSize = true;
             this.LBL1.BackColor = System.Drawing.SystemColors.Info;
-            this.LBL1.Location = new System.Drawing.Point(116, 112);
+            this.LBL1.Location = new System.Drawing.Point(37, 112);
             this.LBL1.Name = "LBL1";
             this.LBL1.Size = new System.Drawing.Size(93, 15);
             this.LBL1.TabIndex = 1;
@@ -218,7 +240,7 @@
             // 
             this.LBL.AutoSize = true;
             this.LBL.BackColor = System.Drawing.SystemColors.Info;
-            this.LBL.Location = new System.Drawing.Point(116, 46);
+            this.LBL.Location = new System.Drawing.Point(38, 46);
             this.LBL.Name = "LBL";
             this.LBL.Size = new System.Drawing.Size(92, 15);
             this.LBL.TabIndex = 0;
@@ -353,6 +375,117 @@
             this.label14.TabIndex = 7;
             this.label14.Text = "label14";
             // 
+            // Radio_GT
+            // 
+            this.Radio_GT.AutoSize = true;
+            this.Radio_GT.Checked = true;
+            this.Radio_GT.Location = new System.Drawing.Point(443, 213);
+            this.Radio_GT.Name = "Radio_GT";
+            this.Radio_GT.Size = new System.Drawing.Size(51, 19);
+            this.Radio_GT.TabIndex = 6;
+            this.Radio_GT.TabStop = true;
+            this.Radio_GT.Text = "Nam";
+            this.Radio_GT.UseVisualStyleBackColor = true;
+            // 
+            // Radio_GT1
+            // 
+            this.Radio_GT1.AutoSize = true;
+            this.Radio_GT1.Location = new System.Drawing.Point(551, 214);
+            this.Radio_GT1.Name = "Radio_GT1";
+            this.Radio_GT1.Size = new System.Drawing.Size(41, 19);
+            this.Radio_GT1.TabIndex = 7;
+            this.Radio_GT1.Text = "Nữ";
+            this.Radio_GT1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(424, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "CMND/CCCD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(424, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Địa Chỉ";
+            // 
+            // txt_cmnd
+            // 
+            this.txt_cmnd.Location = new System.Drawing.Point(424, 75);
+            this.txt_cmnd.Name = "txt_cmnd";
+            this.txt_cmnd.Size = new System.Drawing.Size(301, 23);
+            this.txt_cmnd.TabIndex = 4;
+            this.txt_cmnd.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
+            this.txt_cmnd.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
+            // 
+            // txt_diachi
+            // 
+            this.txt_diachi.Location = new System.Drawing.Point(424, 143);
+            this.txt_diachi.Name = "txt_diachi";
+            this.txt_diachi.Size = new System.Drawing.Size(301, 23);
+            this.txt_diachi.TabIndex = 5;
+            this.txt_diachi.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
+            this.txt_diachi.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Info;
+            this.label5.Location = new System.Drawing.Point(425, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Giới Tính";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.SystemColors.Info;
+            this.label15.Location = new System.Drawing.Point(424, 257);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(52, 15);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "Passport";
+            // 
+            // txt_passport
+            // 
+            this.txt_passport.Location = new System.Drawing.Point(424, 288);
+            this.txt_passport.Name = "txt_passport";
+            this.txt_passport.Size = new System.Drawing.Size(301, 23);
+            this.txt_passport.TabIndex = 8;
+            this.txt_passport.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Control_KeyUp);
+            this.txt_passport.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Validating);
+            // 
+            // err_msg5
+            // 
+            this.err_msg5.AutoSize = true;
+            this.err_msg5.ForeColor = System.Drawing.Color.Red;
+            this.err_msg5.Location = new System.Drawing.Point(510, 46);
+            this.err_msg5.Name = "err_msg5";
+            this.err_msg5.Size = new System.Drawing.Size(38, 15);
+            this.err_msg5.TabIndex = 7;
+            this.err_msg5.Text = "label4";
+            this.err_msg5.Visible = false;
+            // 
+            // err_msg6
+            // 
+            this.err_msg6.AutoSize = true;
+            this.err_msg6.ForeColor = System.Drawing.Color.Red;
+            this.err_msg6.Location = new System.Drawing.Point(475, 112);
+            this.err_msg6.Name = "err_msg6";
+            this.err_msg6.Size = new System.Drawing.Size(38, 15);
+            this.err_msg6.TabIndex = 7;
+            this.err_msg6.Text = "label4";
+            this.err_msg6.Visible = false;
+            // 
             // HanhKhach_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -399,5 +532,16 @@
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label err_msg6;
+        private System.Windows.Forms.Label err_msg5;
+        private System.Windows.Forms.TextBox txt_passport;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_diachi;
+        private System.Windows.Forms.TextBox txt_cmnd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton Radio_GT1;
+        private System.Windows.Forms.RadioButton Radio_GT;
     }
 }
